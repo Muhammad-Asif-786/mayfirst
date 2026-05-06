43526699bd0a394a464366fabcd3d6428ca27fba
Muhammad-Asif-786 <masifmeyo786@gmail.com>
2026-05-06 10:16:15 +0500

    all bugs fixed

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
+          <h4>ALLAH IS GREATEST IN THE WORLD!</h4>
           <h4>When this project  started on First May-2026.!</h4>
           <h4>Today 6th may-2026 it mean one week time consume to complete this project.</h4>
         </div>
