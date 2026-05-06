37a914868e312321cdc40adedf9c3ad404e27d65
Muhammad-Asif-786 <masifmeyo786@gmail.com>
2026-05-06 13:52:03 +0500

    now fix the bug

--- src/App.jsx
+++ src/App.jsx
@@ -15,8 +15,7 @@ function App() {
         </div>
         <div>
           <h4>ALLAH IS GREATEST IN THE WORLD!.</h4>
-          <h4>When this project  started on First May-2026!</h4>
-          <h4>Today 6th may-2026 it mean one week time consume to complete this project..</h4>
+          <h4>All about our the new project</h4>
         </div>
       </section>
     </>
